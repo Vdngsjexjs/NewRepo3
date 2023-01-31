5d67fa8b97f72b9ff4754e2858ddf23679623efc
Minay Ghai <minayghai@Minays-MacBook-Pro.local>
2023-01-31 11:37:23 +0530

    calculator modified

--- Calculator.java
+++ Calculator.java
@@ -1,7 +1,7 @@
 package Classestobetested;
 public class Calculator {
     public int  product(int number1, int number2)
-    {
+    {   System.out.println("hello");
         return number1 * number2;
     }
 }
